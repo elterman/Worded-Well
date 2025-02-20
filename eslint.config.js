a5dbf256381a03010e4aa13624792ce8fd155d97
Arkady Elterman <elterman@gmail.com>
2025-02-20 15:10:40 -0800

    .

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,25 @@ import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
-const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   includeIgnoreFile(gitignorePath),
   js.configs.recommended,
-  ...svelte.configs["flat/recommended"],
+  ...svelte.configs['flat/recommended'],
   {
     languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn'],
+      semi: ['warn', 'always'],
+      'no-multiple-empty-lines': ['warn', { max: 1, maxEOF: 0, }],
+      quotes: ['warn', 'single'],
+    }
   }
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import { windowSize } from '../utils';
+
+  let width = $state(0);
+
+  $effect(() => {
+    const onResize = () => {
+      const { x: wx } = windowSize();
+      width = Math.min(wx * 0.9, 360);
+    };
+
+    onResize();
+
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  });
+</script>
+
+<div class="app">
+  <img src="../src/Images/Title.webp" alt="" {width} />
+  <img src="../src/Images/Intro.webp" alt="" {width} />
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+
+  .app {
+    display: grid;
+    background: #403c38;
+    background-image: url("../src/Images/Stone Wall.webp");
+    background-size: 150px;
+    height: 100vh;
+    place-content: center;
+  }
+</style>
