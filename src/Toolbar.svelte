10f90bc7efec263486b0099ef7287ce272c3b4cd
Arkady Elterman <elterman@gmail.com>
2025-03-09 14:51:16 -0700

    .

--- src/Toolbar.svelte
+++ src/Toolbar.svelte
@@ -1,5 +1,7 @@
 <script>
     import Back from '$lib/images/Back.webp';
+    import ModeEasy from '$lib/images/Mode Easy.webp';
+    import ModeHard from '$lib/images/Mode Hard.webp';
     import ResetStats from '$lib/images/Reset Stats.webp';
     import SoundOff from '$lib/images/Sound Off.webp';
     import SoundOn from '$lib/images/Sound On.webp';
@@ -42,11 +44,16 @@
             playSound('won', { rate: 4 });
         }
     };
+
+    const onMode = () => {
+        _sob.easy = !_sob.easy;
+    };
 </script>
 
 <div class="toolbar">
     <ToolButton src={Back} onClick={onBack} />
     <ToolButton src={Restart} onClick={onSurrender} disabled={_sob.over || !_sob.game_on} />
+    <ToolButton src={_sob.easy ? ModeEasy : ModeHard} onClick={onMode} />
     <ToolButton src={ResetStats} onClick={onResetStats} />
     <ToolButton src={_sound.on ? SoundOn : SoundOff} onClick={onSounds} />
 </div>
