20e712b3d7125317bccef41a9d8dd95b77ad67af
Arkady Elterman <elterman@gmail.com>
2025-03-13 10:36:11 -0700

    .

--- src/Start Page.svelte
+++ src/Start Page.svelte
@@ -10,6 +10,10 @@
     import { _sob, _stats } from './state.svelte';
     import { focusOnApp, later, windowSize } from './utils';
 
+    let width = $state(0);
+    let content = $state(null);
+    let scale = $state(1);
+
     onMount(() => {
         later(() => {
             const json = localStorage.getItem(APP_STATE);
@@ -24,13 +28,20 @@
         }, 2000);
     });
 
-    let width = $state(0);
-
     $effect(() => {
         const onResize = () => {
             const { x: wx } = windowSize();
             width = Math.min(wx * 0.9, 360);
         };
+
+        later(() => {
+            const { y: wy } = windowSize();
+            const pageHi = content.getBoundingClientRect().height + 70;
+
+            if (pageHi > wy) {
+                scale = wy / pageHi;
+            }
+        });
 
         onResize();
         focusOnApp();
@@ -47,14 +58,35 @@
     const style = 'place-self: center; filter: drop-shadow(0 0 5px #000)';
 </script>
 
-<div class="start-page" in:fade={{ duration: 100 }} out:fade={{ duration: 200 }}>
-    <img class="shadow" src={Title} alt="" {width} />
-    <img src={Intro} alt="" {width} />
-    <ImageButton src={Play} width={60} onClick={onPlay} {style} />
-    <div class="version">0313</div>
+<div class="page" in:fade={{ duration: 100 }} out:fade={{ duration: 200 }}>
+    <div class="content" bind:this={content} style="transform: scale({scale})">
+        <img class="shadow" src={Title} alt="" {width} />
+        <img src={Intro} alt="" {width} />
+        <ImageButton src={Play} width={60} onClick={onPlay} {style} />
+        <div class="version">0313</div>
+    </div>
 </div>
 
 <style>
+    .page {
+        height: 100vh;
+        grid-area: 1/1;
+        display: grid;
+        background-image: url('$lib/images/Stone Wall.webp');
+        background-size: 250px;
+        place-content: center;
+        z-index: 100;
+    }
+
+    .content {
+        display: grid;
+        gap: 50px;
+    }
+
+    .shadow {
+        filter: drop-shadow(0 0 5px #000);
+    }
+
     .version {
         display: none;
         position: absolute;
@@ -65,19 +97,5 @@
         padding: 6px 15px 3px;
         border-radius: 50vh;
         font-family: Amnestia;
-    }
-
-    .start-page {
-        grid-area: 1/1;
-        display: grid;
-        gap: 50px;
-        background-image: url('$lib/images/Stone Wall.webp');
-        background-size: 250px;
-        place-content: center;
-        z-index: 100;
-    }
-
-    .shadow {
-        filter: drop-shadow(0 0 5px #000);
     }
 </style>
