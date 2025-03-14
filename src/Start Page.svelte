aa13864991300a45cd7ae786f1ff4a8bd5b4937d
Arkady Elterman <elterman@gmail.com>
2025-03-14 10:49:59 -0700

    .

--- src/Start Page.svelte
+++ src/Start Page.svelte
@@ -64,7 +64,7 @@
         <img src={Intro} alt="" {width} />
         <ImageButton src={Play} width={60} onClick={onPlay} {style} />
     </div>
-    <div class="version">1018</div>
+    <div class="version">1049</div>
 </div>
 
 <style>
